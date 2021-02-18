cde0c9aaa1aad2f3eb362058b71909fcd2531d9a
jschlob <47754528+jschlob@users.noreply.github.com>
2021-02-17 18:21:42 -0700

    Add all files

---
+++ CsvToSqlite/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToSqlite
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new CsvToSqlite()
+            };
+            //ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ CsvToSqlite/Service1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToSqlite
+{
+    public partial class CsvToSqlite : ServiceBase
+    {
+        private String homepath;
+        private String watchpath;
+        public FileSystemWatcher watcher;
+        public CsvToSqlite()
+        {
+            
+            InitializeComponent();
+            
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            
+
+            if (ConfigurationManager.AppSettings.Get("homepath").Equals("") &&
+                ConfigurationManager.AppSettings.Get("watchdirectory").Equals(""))
+            {
+                this.watchpath = "C:\\Users\\diego\\CsvToSqlite\\Convert";
+                this.homepath = "C:\\Users\\diego\\CsvToSqlite";
+                if (!Directory.Exists(homepath))
+                {
+                    Directory.CreateDirectory(homepath);
+                }
+
+                if (!Directory.Exists(watchpath))
+                {
+                    Directory.CreateDirectory(watchpath);
+                }
+                ConfigurationManager.AppSettings.Set("watchdirectory", watchpath);
+                ConfigurationManager.AppSettings.Set("homepath", homepath);
+
+            }
+            if ((!ConfigurationManager.AppSettings.Get("logginglevel").Equals("basic")) && (!ConfigurationManager.AppSettings.Get("logginglevel").Equals("complex")))
+            {
+                LogToFile(DateTime.Now+ " *** Error parsing App.config \'logginglevel\', value must be set to \'basic\' or \'complex\'. Setting logginglevel to \'basic\'");
+                ConfigurationManager.AppSettings.Set("logginglevel", "basic");
+            }
+            this.watcher = new FileSystemWatcher(this.watchpath, "*.*");
+            watcher.Created += OnCreated;
+            watcher.EnableRaisingEvents = true;
+            LogToFile(DateTime.Now + " *** CsvToSqlite service has started");
+
+
+
+        }
+       private void OnCreated(object source, FileSystemEventArgs e) {
+            String filename = e.FullPath;
+            String contents = File.ReadAllText(filename);
+            foreach (String line in contents.Split('\n'))
+            {
+                LogToFile(line.Replace("\n", "")+";");
+            }
+        }
+
+        protected override void OnStop()
+        {       
+            LogToFile(DateTime.Now + " *** CsvToSqlite service has stopped");
+        }
+        public void LogToFile(string Message)
+        {
+            string filepath = this.homepath + "\\Logs\\log.txt";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
