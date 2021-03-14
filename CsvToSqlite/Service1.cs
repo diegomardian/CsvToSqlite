e5bfae9866e472a3c06d985f4d438e8bda9b3897
diegomardian <diegomardian10@gmail.com>
2021-03-13 21:42:28 -0700

    Add column checking and validating

--- CsvToSqlite/Service1.cs
+++ CsvToSqlite/Service1.cs
@@ -28,7 +28,7 @@ namespace CsvToSqlite
         private FileSystemWatcher watcher;
         private String connString;
         private SQLiteConnection conn;
-
+        private bool stopOnError;
 
         public CsvToSqlite()
         {
@@ -40,7 +40,7 @@ namespace CsvToSqlite
 
         protected override void OnStart(string[] args)
         {
-            
+            this.loggingDirectory = "";
             if (ConfigurationManager.AppSettings.Get("homepath").Equals(""))
             {
                 LogToFile("INFO: Config file paramater 'homepath' not set. Setting to C:\\Users\\diego\\CsvToSqlite");
@@ -188,79 +188,7 @@ namespace CsvToSqlite
             {
                 this.loggingLevel = ConfigurationManager.AppSettings.Get("logginglevel");
             }
-            if (ConfigurationManager.AppSettings.Get("logDirectory").Equals(""))
-            {
-                LogToFile("INFO: Config file paramater 'logDirectory' not set. Setting to C:\\Users\\diego\\CsvToSqlite\\Logs");
-                this.loggingDirectory = "C:\\Users\\diego\\CsvToSqlite\\Logs";
-                if (!Directory.Exists(loggingDirectory))
-                {
-                    try
-                    {
-                        Directory.CreateDirectory(loggingDirectory);
-                    }
-                    catch (IOException err)
-                    {
-                        LogToFile(DateTime.Now +
-                                  " CRITICAL ERROR: An error occurred while creating the directory " +
-                                  this.loggingDirectory + ". Try checking the permissions of " + this.homepath +
-                                  " and make sure that Network Service has been granted access.");
-                        if (!logBasic())
-                        {
-                            LogToFile("Error Message\n:" + err.ToString());
-                        }
-
-                        this.Stop();
-                    }
-
-
-                }
-
-            }
-            else
-            {
-                this.loggingDirectory = ConfigurationManager.AppSettings.Get("loggingDirectory");
-                if (!Directory.Exists(loggingDirectory))
-                {
-                    try
-                    {
-                        Directory.CreateDirectory(loggingDirectory);
-                    }
-                    catch (IOException err)
-                    {
-                        LogToFile(DateTime.Now +
-                                  " CRITICAL ERROR: An error occurred while creating the directory " +
-                                  this.loggingDirectory + ". Try checking the permissions of " + this.homepath +
-                                  " and make sure that Network Service has been granted access.");
-                        if (!logBasic())
-                        {
-                            LogToFile("Error Message\n:" + err.ToString());
-                        }
-
-                        this.Stop();
-                    }
-
-                }
-
-            }
-            if (Directory.Exists(this.homepath + "\\Logs"))
-            {
-                LogToFile("INFO: Creating " + this.homepath + "\\Logs");
-                try
-                {
-                    Directory.CreateDirectory(this.homepath + "\\Logs");
-                }
-                catch (IOException err)
-                {
-                    LogToFile(DateTime.Now + " CRITICAL ERROR: An error occurred while creating the directory " + this.homepath + "\\Logs. Try checking the permissions of "+ this.homepath+" and make sure that Network Service has been granted access.");
-                    if (!logBasic())
-                    {
-                        LogToFile("Error Message\n:" + err.ToString());
-                    }
-                    this.Stop();
-                }
-
-
-            }
+            
             try { 
                 this.parserConfig = JsonConvert.DeserializeObject<Dictionary<String, Object>>(File.ReadAllText(this.parserConfigFile));
             }
@@ -297,8 +225,26 @@ namespace CsvToSqlite
                 this.Stop();
             }
 
-          
-            
+            if (!this.parserConfig.ContainsKey("columns"))
+            {
+                LogToFile(DateTime.Now + " CRITICAL ERROR: Please include a 'columns' key in your parser config file. Quiting...");
+                this.Stop();
+            }
+            if (!this.parserConfig.ContainsKey("stopOnError"))
+            {
+                LogToFile(DateTime.Now + " ERROR: Could not find the key 'stopOnError' in your parser config file. Setting to 'True'.");
+                this.stopOnError = true;
+            }
+            else
+            {
+                if (!(this.parserConfig["stopOnError"].ToString().Equals("False")) || (this.parserConfig["stopOnError"]).ToString().Equals("True"))
+                {
+                    LogToFile(DateTime.Now + " ERROR: Parser config file 'stopOnError' should be either 'True' or 'False'. Setting to 'True'.");
+                    this.stopOnError = true;
+                }
+            }
+
+
             this.watcher = new FileSystemWatcher(this.watchpath, "*.*");
             watcher.Created += OnCreated;
             watcher.EnableRaisingEvents = true;
@@ -329,27 +275,70 @@ namespace CsvToSqlite
                 if (Parser.hasDuplicate((List<String>)csv["headers"]))
                 {
                     String distinctColumns = "";
-                    for (int i = 0; i < ((List<String>)csv["headers"]).Distinct().Count(); i++) { 
-                        distinctColumns += ((List<String>)csv["headers"]).Distinct().ToArray()[i]+", ";
+                    if (((List<String>)csv["headers"]).Distinct().Count() == 1)
+                    {
+                        distinctColumns = "column " + ((List<String>)csv["headers"]).Distinct().ToArray()[0];
                     }
-                    LogToFile(DateTime.Now + " PARSE ERROR: Found duplicates for columns "+ distinctColumns + ". Stopping parsing...");
+                    else if (((List<String>)csv["headers"]).Distinct().Count() == 2)
+                    {
+                        distinctColumns += "columns " + ((List<String>)csv["headers"]).Distinct().ToArray()[0] + " and " + ((List<String>)csv["headers"]).Distinct().ToArray()[1];
+                    }
+                    else { 
+                        distinctColumns += "columns ";
+                        for (int i = 0; i < ((List<String>)csv["headers"]).Distinct().Count(); i++) { 
+                            if (i == ((List<String>)csv["headers"]).Distinct().Count()-1)
+                                distinctColumns += "and "+((List<String>)csv["headers"]).Distinct().ToArray()[i];
+                            else
+                                distinctColumns += ((List<String>)csv["headers"]).Distinct().ToArray()[i]+", ";
+                        }
+                    }
+                    LogToFile(DateTime.Now + " PARSE ERROR: Found duplicates for "+ distinctColumns + ". Stopping parsing...");
+                    return;
+                }
+                if (!(((List<String>)csv["headers"]).Count == ((Dictionary<String, Object>)this.parserConfig["columns"]).Count))
+                {
+                    LogToFile(DateTime.Now + " PARSE ERROR: Found " + ((List<String>)csv["headers"]).Count + " columns in the header of "+filename+" but it should have " + ((Dictionary<String, Object>)this.parserConfig["columns"]).Count + " columns. Stopped parsing " + filename + ".");
                     return;
                 }
                 for (int i = 0; i < ((List<List<String>>)csv["data"]).Count; i++)
                 {
-                    if (!(((List<List<String>>)csv["data"])[i].Count == ((List<List<String>>)csv["header"]).Count))
+                    if (!(((List<List<String>>)csv["data"])[i].Count == ((List<String>)csv["headers"]).Count))
                     {
-                        LogToFile(DateTime.Now + " PARSE ERROR: Row  " + i + " has "+ ((List<List<String>>)csv["data"])[i].Count + ". Stopped parsing "+filename+".");
+                        LogToFile(DateTime.Now + " PARSE ERROR: Row  " + i + " has "+ ((List<List<String>>)csv["data"])[i].Count + " columns while it should have "+ ((Dictionary<String, Object>)this.parserConfig["columns"]).Count + " columns. Stopped parsing "+filename+".");
                         return;
                     }
                 }
-                
+                for (int i = 0; i < ((List<String>)csv["headers"]).Count; i++)
+                {
+                    if (!((Dictionary<String, Object>)this.parserConfig["columns"]).ContainsKey(((List<String>)csv["headers"])[i]))
+                    {
+                        LogToFile(DateTime.Now+" PARSE ERROR: Column " + ((List<String>)csv["header"])[i] + " does not match any column defined in "+this.parserConfigFile);
+                        return;
+                    }
+                }
+                String columnNames = "";
+                for (int i = 0; i < ((List<String>)csv["headers"]).Count; i++)
+                {
+                    if (i == ((List<String>)csv["headers"]).Count-1)
+                    {
+                        columnNames += ((List<String>)csv["headers"])[i];
+                    }
+                    columnNames += ((List<String>)csv["headers"])[i] + ",";
+                }
                 foreach (List<String> line in (List<List<String>>)csv["data"])
                 {
-                    foreach (String letter in line) {
-                        LogToFile(letter+"\n");
+                    String values = "";
+                    for (int i = 0; i < line.Count; i++)
+                    {
+                        if (i == line.Count-1)
+                        {
+                            values += line[i];
+                        }
+                        else { 
+                            values += line[i]+",";
+                        }
                     }
-                    cmd.CommandText = "INSERT INTO CsvToSqlite(Age, Email, Name) VALUES('"+line[0]+"','"+line[1]+ "','" + line[2]+"')";
+                    cmd.CommandText = "INSERT INTO CsvToSqlite("+columnNames+") VALUES('"+line[0]+"','"+line[1]+ "','" + line[2]+"')";
                     cmd.ExecuteNonQuery();
 
                 }
@@ -371,7 +360,44 @@ namespace CsvToSqlite
         }
         public void LogToFile(string Message)
         {
-            string filepath = this.homepath + "\\Logs\\log.txt";
+            if (ConfigurationManager.AppSettings.Get("logDirectory").Equals("") && this.loggingDirectory.Equals(""))
+            {
+                this.loggingDirectory = "C:\\Users\\diego\\CsvToSqlite\\Logs";
+                if (!Directory.Exists(loggingDirectory))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(loggingDirectory);
+                    }
+                    catch (IOException err)
+                    {
+                        
+
+                        this.Stop();
+                    }
+
+
+                }
+
+            }
+            else
+            {
+                this.loggingDirectory = ConfigurationManager.AppSettings.Get("loggingDirectory");
+                if (!Directory.Exists(loggingDirectory))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(loggingDirectory);
+                    }
+                    catch (IOException err)
+                    {
+                        this.Stop();
+                    }
+
+                }
+
+            }
+            string filepath = this.loggingDirectory;
             if (!File.Exists(filepath))
             {
                 using (StreamWriter sw = File.CreateText(filepath))
